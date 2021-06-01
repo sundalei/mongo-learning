96f7ec871c02de61ba6295873a9c818eb390693c
Leo Sun <leosun@Leos-MacBook-Pro.local>
2021-06-01 20:13:17 +0800

    add new template

--- src/main/java/com/test/mongotest/HelloController.java
+++ src/main/java/com/test/mongotest/HelloController.java
@@ -1,13 +1,15 @@
 package com.test.mongotest;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMethod;
 
-@RestController
+@Controller
+@RequestMapping("/page")
 public class HelloController {
 
-    @RequestMapping("/")
-    public String index() {
-        return "Greetings from Spring Boot!";
+    @RequestMapping(value = "/new", method = RequestMethod.GET)
+    public String create() {
+        return "new";
     }
 }
